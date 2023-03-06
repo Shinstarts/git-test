febfa2cd88c93dac54dc38172342a9766d10f880
Shinstarts <127072860+Shinstarts@users.noreply.github.com>
2023-03-06 16:46:10 +0800

    pull commit

--- src/main/java/com/geren/Git.java
+++ src/main/java/com/geren/Git.java
@@ -10,5 +10,6 @@ public class Git {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("push2 test!");
+        System.out.println("pull test!");
     }
 }
